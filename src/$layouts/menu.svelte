bb070377cc1a3a47dd72195de305bd8bb644eed8
unknown <ricardo.caballero@alumno.buap.mx>
2022-06-01 14:23:45 -0500

    [Layout](Navbar) Se actualizo la barra de navegacion, aun quedan cambios por hacer

--- src/$layouts/menu.svelte
+++ src/$layouts/menu.svelte
@@ -34,18 +34,41 @@
 </style>
 
 
-    <div class="columns is-centered">
-        <div class="column is-half ">
+    <!-- <div class="columns is-centered">
+        
             <div class="nav">
                 <div class="title"> Tareas de { $UserStore.name  }</div>
-                <div class="columns is-centered">
                     <Button on:click={resume} icon="chart-line" />
                     <Button on:click={tasks} icon="list-check" />
                     <Button on:click={ logout } icon="person-running" />
-
-                </div>
-
             </div>
 
-        </div>  
+    </div> -->
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+           
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+                <Button on:click={tasks} icon="list-check" />
+            </a>
+      
+            <a class="navbar-item">
+                <Button on:click={resume} icon="chart-line" />
+            </a>
+      
+          </div>
+          <div class="navbar-end">
+            <div class="navbar-item">
+                <Button on:click={ logout } icon="person-running" />
+
+            </div>
+          </div>
+        </div>
+    </nav>
+    <div class="center">
+        <div class="title is-3 "> Tareas de { $UserStore.name  }</div>
     </div>
